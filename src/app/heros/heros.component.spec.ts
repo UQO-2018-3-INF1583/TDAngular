e597d8bf3897cb1b2edd722d0fa3238877ca2745
levasseurv <levv13@uqo.ca>
2018-09-16 21:12:09 -0400

    Une solution au TD3 portant sur les tests unitaires avec Angular et Jasmine

--- src/app/heros/heros.component.spec.ts
+++ src/app/heros/heros.component.spec.ts
@@ -1,14 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HEROS } from './mock-heros';
 import { HerosComponent } from './heros.component';
+import {FormsModule} from '@angular/forms';
+import {HerosService} from './heros.service';
+import {of} from 'rxjs/observable/of';
 
 describe('HerosComponent', () => {
   let component: HerosComponent;
   let fixture: ComponentFixture<HerosComponent>;
+  let service: HerosService;
 
   beforeEach(async(() => {
+
+    // Créer un faux service avec un espion sur la méthode getHeros
+    const fauxService = jasmine.createSpyObj('HerosService', ['getHeros']);
+    // Make the spy return a synchronous Observable with the test data
+    const getHerosSpy = fauxService.getHeros.and.returnValue( of(HEROS) );
+
     TestBed.configureTestingModule({
-      declarations: [ HerosComponent ]
+      declarations: [ HerosComponent ],
+      imports : [ FormsModule ],
+      // providers: [HerosService]
+      providers: [{ provide: HerosService, useValue: fauxService }] // utiliser le faux service
     })
     .compileComponents();
   }));
@@ -22,4 +35,11 @@ describe('HerosComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have heros after Angular calls ngOnInit', () => {
+    component.ngOnInit();
+    service = TestBed.get(HerosService);
+    expect(component.heros).toBeTruthy(service.getHeros());
+  });
+
 });
--- src/app/heros/heros.service.spec.ts
+++ src/app/heros/heros.service.spec.ts
@@ -1,8 +1,17 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { HerosService } from './heros.service';
+import {Heros} from './heros';
+import { HEROS } from './mock-heros';
 
 describe('HerosService', () => {
+  let service: HerosService;
+  /*const HEROS: Heros[] = [
+    { id: 11, nom: 'Mr. Nice' },
+    { id: 1, nom: 'Windstorm'},
+    { id: 15, nom: 'Magneta'},
+  ];*/
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [HerosService]
@@ -12,4 +21,23 @@ describe('HerosService', () => {
   it('should be created', inject([HerosService], (service: HerosService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('getHeros should return mock-heros content', () => {
+    service = TestBed.get(HerosService);
+    expect(service.getHeros()).toBeTruthy(HEROS);
+  });
+});
+
+describe('More HerosService', () => {
+  let service: HerosService;
+  /*const HEROS: Heros[] = [
+    { id: 11, nom: 'Mr. Nice' },
+    { id: 1, nom: 'Windstorm'},
+    { id: 15, nom: 'Magneta'},
+  ];*/
+  beforeEach(() => { service = new HerosService(); });
+
+  it('getHeros should return mock-heros content', () => {
+    expect(service.getHeros()).toBeTruthy(HEROS);
+  });
 });
